30a4a18a787e6bbecc7b6f408533b00398e34eec
DanielValleMillares <da.valle@isciii.es>
2021-10-07 14:32:33 +0200

    Added dataset documentations

---
+++ R/ids.R
@@ -0,0 +1,11 @@
+#' Data of non-coding RNAs IDs
+#'
+#' The ids is a list object that includes 4 slots named according to different non-coding RNA types.
+#' @format A list with 4 slots. Each slot has a vector of non-coding RNAs IDs:
+#' \describe{
+#'   \item{miRNAs}{Human (Homo sapiens) miRNAs}
+#'   \item{lncRNAs}{Human (Homo sapiens) lncRNAs}
+#'   \item{snRNAs}{Mouse (Mus musculus) snRNAs}
+#'   \item{snoRNAs}{Mouse (Mus musculus) snoRNAs}
+#' }
+"ids"
